b59af72fa1613ce880ab39d2d8a6dc8d5a6deac1
Prasad Muddappa <prasad.cm@sonata-software.com>
2026-06-16 16:28:18 +0530

    Add Github pages deployment

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,20 @@
-import type { StorybookConfig } from '@storybook/vue3-vite';
+import type { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
     "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
+    "@storybook/addon-onboarding",
   ],
-  "framework": "@storybook/vue3-vite"
+  framework: "@storybook/vue3-vite",
+
+  async viteFinal(config) {
+    config.base = "/storybook-vue-poc/";
+    return config;
+  },
 };
+
 export default config;
